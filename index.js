2774a5925457561f5378ad6b4a31f8151edc9090
thousand0001 <thousand0001@thousand0001de-MacBook-Air.local>
2017-06-02 04:43:21 +0800

    dqimongo second test

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
 //*
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-MongoClient.connect('mongodb://thousand0001:mm570129@ds153730.mlab.com:53730/heroku_tmgjg46t/dqi', function(err,db){
-
+MongoClient.connect('mongodb://thousand0001:mm570129@ds153730.mlab.com:53730/heroku_tmgjg46t', function(err,db){
 	var equipments = db.collection('equipments');
 	equipments.insert({
-		"eqId": "dqiG01",
+		"eqId": "dqiG02",
 		"bindingQty" : 1,
 		"bindingMax" : 4,
-		"valveId" : "dqiV01" 
+		"valveId" : "dqiV02" 
 		}, function(err, docs){
 		if (err) {
 		console.log('insert err');
