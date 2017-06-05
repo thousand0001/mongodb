6fe95caaaa2f64ee58a3be404a007dbec7e4d952
thousand0001 <thousand0001@gmail.com>
2017-06-05 18:41:45 +0800

    static file test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 //app.listen(3000);
-var port = process.env.PORT || 3000;
+var port =  3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
