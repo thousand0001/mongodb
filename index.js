29cf98b5111e52f4f993eaf33901640ba20fb7cf
thousand0001 <thousand0001@gmail.com>
2017-06-05 20:10:46 +0800

    static file test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mytimezone = require("./myModule/mytimeZone.js");
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -12,6 +13,7 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log("Now time :"+mytimezone.TPI());
 });
  /*
 var port = process.env.PORT || 3000;
---
+++ myModule/myTimeZone.js
@@ -0,0 +1,30 @@
+//function timezone(){//"Asia/Taipei"
+exports.TPI = function(){
+var d = new Date();
+localTime = d.getTime();
+localOffset = d.getTimezoneOffset() * 60000;
+utc = localTime + localOffset;
+console.log("UTC: "+utc);
+TPIoffset = 8;   
+TPI = utc + (3600000*TPIoffset);
+nd = new Date(TPI);
+APM="上午";
+// Hours part from the timestamp
+var hours = Number(nd.getHours());
+if (hours > 12){
+	hours-=12;
+	APM="下午";
+	}
+else if (hours == 12){
+	APM="中午";
+	}	
+// Minutes part from the timestamp
+var minutes = "0" + nd.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + nd.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = APM + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+	return (formattedTime);
+}
+//}
