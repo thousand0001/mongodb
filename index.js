6d0822b55d8ae5a6b21573c8efb609350eb2abc1
thousand0001 <thousand0001@gmail.com>
2017-06-05 20:33:31 +0800

    static file test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var mytimezone = require("./mymodule/mytimezone.js");
+var mytimezone = require("./module/mytimezone.js");
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
