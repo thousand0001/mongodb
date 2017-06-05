873d6e6871df1cf01d0debbf40c72e46ba201f22
thousand0001 <thousand0001@gmail.com>
2017-06-05 16:13:58 +0800

    static file test

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 //var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+//app.listen(3000);
  /*
 
 require ('./lib/db');
