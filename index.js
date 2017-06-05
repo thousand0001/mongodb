5f731674da5af484567d1e06dbd4e8d24e2b012a
thousand0001 <thousand0001@gmail.com>
2017-06-05 18:36:55 +0800

    static file test

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+app.listen(3000);
+
+ /*
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-//app.listen(3000);
- /*
+
 
 require ('./lib/db');
 var mongoose = require('mongoose');
