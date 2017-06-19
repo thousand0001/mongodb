f7eabf05a4b7476d7034ae6912ec63ef534aba7d
thousand0001 <thousand0001@gmail.com>
2017-06-19 20:10:29 +0800

    test facebook bot

--- dqimongo.js
+++ dqimongo.js
@@ -26,7 +26,7 @@ app.get('/webhook', function(req, res) {
       req.query['hub.verify_token'] === facebooktoken) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
-  } else {
+  } else { 
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);
   }
