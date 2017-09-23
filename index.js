332f0dbe1b5e9d1a645db21773d8ba476ea427a5
thousand0001 <thousand0001@gmail.com>
2017-09-23 15:36:55 +0800

    mosca mqtt upload

--- index.js
+++ index.js
@@ -1,93 +1,77 @@
-/*
-var http = require("http");
-var io = require('socket.io'); // 加入 Socket.IO
-var express = require ('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-
-var server = http.createServer(function(request, response) {
-console.log('Connection');
-});
-app.listen('8080');
-server.listen(8001);
-
-//io.listen(server); // 開啟 Socket.IO 的 listener
-var serv_io = io.listen(server);
-
-serv_io.sockets.on('connection', function(socket) {
-socket.emit('message', {'message': 'hello world'});
-setInterval(function() {
-socket.emit('date', {'date': new Date()});
-}, 1000);
-});
-///////////
-
-var io = require('socket.io')();
-io.on('connection', function(socket){
-  socket.emit('message', {'message': 'hello world'});
-  setInterval(function() {
-    socket.emit('date', {'date': new Date()});
-  }, 1000);
-});
-io.listen(8001);
-/////////////
-app.set('port', (process.env.PORT || 5000));
-*/
-
+// Setup basic express server
 var express = require('express');
 var app = express();
+var path = require('path');
 var server = require('http').createServer(app);
+// var io = require('../..')(server);
 var io = require('socket.io')(server);
-app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
 
-io.on('connection', function(socket){
-  console.log("connection");
-  socket.on('event', function(data){
-    console.log("data received;");
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
 
-  socket.emit("'message', {'message': 'hello world'}");
-  setInterval(function() {
-    socket.emit("'date', {'date':Date( Date.now())}") ;//Date.setUTCHours()
-  }, 2000);
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
-server.listen(process.env.PORT || 8005);
-app.get('/', function(request, response) {
-  response.send('Hello World!!!!');
-});
-
-/////////////////////////
-/*
-var ascoltatore = {
-  //using ascoltatore
-  type: 'mongo',
-  // url: 'mongodb://localhost:27017/mqtt',
-  url: 'mongodb://thousand0001:mm570129@ds153730.mlab.com:53730/heroku_tmgjg46t',
-  pubsubCollection: 'ascoltatori',
-  mongo: {}
-};
-
-var settings = {
-  port: 1883,
-  backend: ascoltatore
-};
-
-var server = new mosca.Server(settings);
-
-server.on('clientConnected', function(client) {
-    console.log('client connected', client.id);
-});
-
-// fired when a message is received
-server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
-});
-
-server.on('ready', setup);
-
-// fired when the mqtt server is ready
-function setup() {
-  console.log('Mosca server is up and running');
-}
-*/
