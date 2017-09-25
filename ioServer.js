8aa34b329c2b28ab6628a12bc299398190dcff43
thousand0001 <thousand0001@gmail.com>
2017-09-25 22:24:37 +0800

    ws upload

--- ioServer.js
+++ ioServer.js
@@ -13,11 +13,11 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws, req) {
-  const location = url.parse(req.url, true);
-  // You might use location.query.access_token to authenticate or share sessions
-  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-  const ip = req.connection.remoteAddress;
-  console.log('ip:'+ip.toString());
+  // const location = url.parse(req.url, true);
+  // // You might use location.query.access_token to authenticate or share sessions
+  // // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+  // const ip = req.connection.remoteAddress;
+  // console.log('ip:'+ip.toString());
 
   ws.on('message', function incoming(msg) {
     var obj=JSON.parse(msg);
