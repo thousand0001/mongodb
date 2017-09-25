198c3073c8de2e49dedf0e827398e36e2c4ff0f5
thousand0001 <thousand0001@gmail.com>
2017-09-25 22:45:14 +0800

    ws upload

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
+const express = require('express');
 const http = require('http');
-var path = require('path');
-var server = http.createServer(app);
 const url = require('url');
 const WebSocket = require('ws');
+var path = require('path');
+
+const app = express();
+
+app.use(function (req, res) {
+  res.send({ msg: "hello" });
+});
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws, req) {
   const location = url.parse(req.url, true);
   // You might use location.query.access_token to authenticate or share sessions
   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-  const ip = req.connection.remoteAddress;
-  console.log('ip:'+ip.toString());
 
-  wss.on('message', function incoming(msg) {
-    // var obj=JSON.parse(msg);
-    // console.log('receive name: %s', obj.name);
-    // console.log('receive company: %s', obj.company);
-    console.log('received  : %s', msg);
-    wss.send(msg);
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send(message);
   });
-//   var msg =  JSON.stringify({
-//     'name': 'luke',
-//     'company': '0987654321',
-//     'time':Date.now()
-// });
+
 });
-// var io = require('../..')(server);
+
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
--- ioServer.js
+++ ioServer.js
@@ -2,39 +2,32 @@ const express = require('express');
 const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
+var path = require('path');
 
 const app = express();
 
 app.use(function (req, res) {
   res.send({ msg: "hello" });
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(wss, req) {
-  // const location = url.parse(req.url, true);
-  // // You might use location.query.access_token to authenticate or share sessions
-  // // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-  // const ip = req.connection.remoteAddress;
-  // console.log('ip:'+ip.toString());
+wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
-  wss.on('message', function incoming(msg) {
-    // var obj=JSON.parse(msg);
-    // console.log('receive name: %s', obj.name);
-    // console.log('receive company: %s', obj.company);
-    // console.log('received time : %s', obj.time);
-    console.log('received message:%s', msg);
-    wss.send(msg);
-
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send(message);
   });
-//   var msg =  JSON.stringify({
-//     'name': 'luke',
-//     'company': '0987654321',
-//     'time':Date.now()
-// });
+
 });
 
-server.listen(8000, function listening() {
-  console.log('Listening on %d', server.address().port);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
