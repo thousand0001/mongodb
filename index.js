b83a0603075c90e9852cdc601f6d1546dd19ab35
thousand0001 <thousand0001@gmail.com>
2017-09-25 21:34:42 +0800

    ws upload

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
+const http = require('http');
 var path = require('path');
-var server = require('http').createServer(app);
+var server = http.createServer(app);
+const url = require('url');
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+  const ip = req.connection.remoteAddress;
+  console.log('ip:'+ip.toString());
+
+  ws.on('message', function incoming(msg) {
+    var obj=JSON.parse(msg);
+    console.log('receive name: %s', obj.name);
+    console.log('receive company: %s', obj.company);
+    console.log('received time : %s', obj.time);
+  });
+  var msg =  JSON.stringify({
+    'name': 'luke',
+    'company': '0987654321',
+    'time':Date.now()
+});
+  ws.send(msg);
+});
 // var io = require('../..')(server);
 var port = process.env.PORT || 3000;
 
