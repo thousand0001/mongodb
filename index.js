d2bd2aff0ed973bb382256e623d5e1eed55159b6
thousand0001 <thousand0001@gmail.com>
2017-09-26 20:25:08 +0800

    ws upload

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ wss.on('connection', function connection(ws, req) {
   });
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send('received: '+ message+Date.now().toString());
+    ws.send('received: '+ message+' '+Date.now().toString());
     // ws.send(message);
   });
 
