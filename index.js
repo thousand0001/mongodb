b2c1284ace4a54706cbd1e30a20cf8b125608002
thousand0001 <thousand0001@gmail.com>
2017-09-26 17:37:53 +0800

    ws upload

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ wss.on('connection', function connection(ws, req) {
   const location = url.parse(req.url, true);
   // You might use location.query.access_token to authenticate or share sessions
   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
+  ws.on('open',function open(){
+    console.log('ws open');
+  });
+  ws.on('close', function close() {
+    console.log('close');
+  });
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
     // ws.send(message);
