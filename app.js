dac58cf9f983dbc87012eef8900907bef4fa45fb
thousand0001 <thousand0001@gmail.com>
2017-10-21 21:20:35 +0800

    github

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path=require('path');
+var app = express();
+server  = require('http').Server(app);
+app.set('views',__dirname);    // 设置视图
+app.set('view engine', 'html');
+app.engine( '.html', require( 'ejs' ).__express );
+require('./index1')(app);      //路由配置文件
+server.listen(8088,function(){
+console.log('App start,port 80.');
+});
---
+++ index1.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+app.get('/',function(req,res){
+res.render('test',{issuccess:"successful"})
+});
+app.get('/interface',function(req,res){});
+app.post('/interface',function(req,res){});
+}
